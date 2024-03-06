0944ff838c79afb425dd8ac5da133264577605e0
lel20 <leonardoparedes.v.r@gmail.com>
2024-03-06 11:00:39 +0100

    Version Final

--- menu.py
+++ menu.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 16 21:03:56 2022
-
-@author: leonardo
-"""
 import streamlit as st
 st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
